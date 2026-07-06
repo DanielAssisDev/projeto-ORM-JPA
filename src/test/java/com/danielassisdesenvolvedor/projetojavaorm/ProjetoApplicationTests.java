225f5f3c3ee00a9d8612c3ecc13b8fbd4a9b3690
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-07-05 21:28:46 -0300

    Projeto criado e inicializado

---
+++ src/test/java/com/danielassisdesenvolvedor/projetojavaorm/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielassisdesenvolvedor.projetojavaorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
